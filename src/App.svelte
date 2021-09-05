24a1ed469f7fa3b12ba653abb1588c5eccdee8c4
Josef Mueller <j.mueller5021@gmail.com>
2021-09-05 14:26:34 +0200

    :sparkles: added `svelte`

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+    import Nav from "./components/Nav.svelte";
+
+    console.log('66c28bfe@protonmail.com')
+</script>
+
+<main>
+    <Nav/>
+    <section class="section" id="about" role="contentinfo" aria-label="section" aria-labelledby="about Josef Müller">
+        <h1 aria-labelledby="greeting">
+            Hello!
+        </h1>
+        <h3 aria-labelledby="introduction">
+            My name is Josef Müller.
+        </h3>
+        <p>
+            I study Applied Computer Science with focus on Embedded Systems at the HTWG Konstanz. In my spare time I work on small projects and like to experiment with new technologies.
+            My interests lie especially in the frontend area, where I like to work with frameworks like Vue and Svelte.
+            Furthermore I write projects in Python, Scala, Java and C from time to time.
+        </p>
+        <p>
+            If you are interested in starting something with me or you have questions about a project feel free to contact me.
+        </p>
+    </section>
+</main>
+
+<style>
+    main {
+        text-align: left;
+        max-width: none;
+        padding: 1em;
+        margin: 0 auto;
+        font-size: x-large;
+    }
+
+    @media (max-width: 100px) {
+        main {
+            font-size: xx-small;
+        }
+    }
+
+    :global(a, a:visited) {
+        color: black;
+    }
+
+    .shadow {
+        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    }
+</style>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,54 @@
+<nav role="navigation" aria-label="main navigation">
+    <ul class="nav-ul">
+        <li class="nav-li">
+            <a href="/#about">About Me</a>
+        </li>
+        <li class="nav-li">
+            <a href="https://github.com/jpkmiller" target="_blank">Projects</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        border: thin solid;
+        background: white;
+    }
+
+    .nav-ul {
+        padding: 0;
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    /* when screen is very very small */
+    @media (max-width: 160px) {
+        .nav-ul {
+            display: none;
+        }
+    }
+
+    /* change to list */
+    @media (max-width: 280px) {
+        .nav-ul {
+            flex-direction: column;
+        }
+    }
+
+    @media (min-width: 540px) {
+        .nav-ul {
+            justify-content: flex-start;
+        }
+
+        .nav-li {
+            margin-left: 10px;
+            margin-right: 10px;
+        }
+    }
+
+    li {
+        list-style: none;
+        margin: auto;
+    }
+
+</style>
