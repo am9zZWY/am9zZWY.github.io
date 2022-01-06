9fe28b9cd798990612e8642715d7f7fbd55795b3
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-01-06 16:55:08 +0100

    :sparkles: App.svelte: added portrait and Projects.svelte.

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,36 @@
 <script>
     import Nav from "./components/Nav.svelte";
-
+    import Projects from "./components/Projects.svelte";
     console.log('66c28bfe@protonmail.com')
 </script>
 
 <main>
     <Nav/>
-    <section class="section" id="about" role="contentinfo" aria-label="section" aria-labelledby="about Josef Müller">
-        <h1 aria-labelledby="greeting">
-            Hello!
-        </h1>
-        <h3 aria-labelledby="introduction">
-            My name is Josef Müller.
-        </h3>
-        <p>
-            I study Applied Computer Science with focus on Embedded Systems at the HTWG Konstanz. In my spare time I work on small projects and like to experiment with new technologies.
-            My interests lie especially in the frontend area, where I like to work with frameworks like Vue and Svelte.
-            Furthermore I write projects in Python, Scala, Java and C from time to time.
-        </p>
-        <p>
-            If you are interested in starting something with me or you have questions about a project feel free to contact me.
-        </p>
+    <section class="section" id="about" role="contentinfo" aria-label="section" aria-labelledby="About Josef Müller">
+        <div class="container">
+            <div>
+                <h1 aria-labelledby="greeting">
+                    Hello!
+                </h1>
+                <h3 aria-labelledby="introduction">
+                    My name is Josef Müller.
+                </h3>
+                <p class="text">
+                    I study Applied Computer Science with focus on Embedded Systems at the HTWG Konstanz. In my spare time I work on small projects and like to experiment with new technologies.
+                    My interests lie especially in the frontend area, where I like to work with frameworks like Vue and Svelte.
+                    Furthermore I write projects in Python, Scala, Java and C from time to time.
+                </p>
+                <p class="text">
+                    If you are interested in starting something with me or you have questions about a project feel free
+                    to
+                    contact me.
+                </p>
+            </div>
+            <img class="img portrait" alt="Portrait of Josef" src="https://avatars.githubusercontent.com/u/46693545?v=4"/>
+        </div>
+    </section>
+    <section class="section" id="projects" role="contentinfo" aria-label="section" aria-labelledby="Projects of Josef Müller">
+        <Projects />
     </section>
 </main>
 
@@ -39,11 +49,17 @@
         }
     }
 
+    .text {
+        line-height: 35px;
+    }
+
+    .container {
+        margin-top: 20px;
+        display: flex;
+    }
+
     :global(a, a:visited) {
         color: black;
     }
 
-    .shadow {
-        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-    }
 </style>
