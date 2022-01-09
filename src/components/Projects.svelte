b4db641e6c3a81205f145880d4c9d20ccb28af24
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-01-09 01:29:36 +0100

    :sparkles: Projects.svelte: added filter and username as prop.

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,23 +1,29 @@
 <script>
     import ProjectCard from "./ProjectCard.svelte";
 
+    export let excludedLanguages = ['', 'CSS'];
+    export let githubName = 'jpkmiller';
+
     let fetchedRepos = [];
-    fetch('https://api.github.com/users/jpkmiller/repos')
+    fetch(`https://api.github.com/users/${githubName}/repos`)
         .then((response) => response.json())
         .then((repos) =>
-                fetchedRepos = repos.filter(({archived}) => !archived).map(({name, full_name, description, html_url}) => {
-                return {
+            fetchedRepos = repos
+                .filter(({
+                             archived,
+                             language
+                         }) => !archived && language !== null && !excludedLanguages.includes(language))
+                .map(({name, full_name, description, html_url}) => ({
                     fullName: full_name,
                     name: name,
                     description: description,
                     url: html_url
-                }
-            })
+                }))
         )
 </script>
 
 <div>
     {#each fetchedRepos as repo }
-        <ProjectCard {...repo} />
+        <ProjectCard {...repo}/>
     {/each}
 </div>
